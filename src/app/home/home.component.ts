2c609a07552cc65af463f38bef4b130f463f64a7
Mike MA <594mjf@gmail.com>
2019-09-21 21:52:54 +0800

    feat(homecomponent): add Google Map

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var google;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.scss']
 })
-export class HomeComponent {
-  
-  isCollapsed = false;
+export class HomeComponent implements OnInit {
+
   isSearching = false;
   startPoint: string;
   endPoint: string;
-  
+
+  // @ViewChild('map', {static: false}) map;
+
   constructor() {
-  
   }
-  
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+
   searchRoute() {
     this.isSearching = true;
+  }
+
+  resetSearch() {
+    this.startPoint = '';
+    this.endPoint = '';
+    this.isSearching = false;
+  }
+
+  initMap() {
+    // console.log('get map element', document.getElementById('myMap'));
+    const map = new google.maps.Map(document.getElementById('myMap'), {
+      center: {lat: 22.28552, lng: 114.15769},
+      zoom: 13
+    });
+
   }
 }
